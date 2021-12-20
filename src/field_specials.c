6378c8c6027d6161b3580bf4db99affd8332c402
lemondrop81 <hacker814@hotmail.com>
2021-12-20 08:31:22 -0600

    Change menu length of the region names

--- src/field_specials.c
+++ src/field_specials.c
@@ -2335,7 +2335,7 @@ void ShowScrollableMultichoice(void)
             break;
         case SCROLL_MULTI_REGION_NAMES:
             task->tMaxItemsOnScreen = MAX_SCROLL_MULTI_ON_SCREEN;
-            task->tNumItems = 7;
+            task->tNumItems = 8;
             task->tLeft = 22;
             task->tTop = 1;
             task->tWidth = 12;
