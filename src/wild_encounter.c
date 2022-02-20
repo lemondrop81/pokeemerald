b302f439f0cdcb82cc640283d273f8b11256ca56
lemondrop81 <hacker814@hotmail.com>
2022-02-20 10:37:05 -0600

    Checks the player levels and adjusts

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -420,6 +420,80 @@ static bool8 TryGenerateWildMon(const struct WildPokemonInfo *wildMonInfo, u8 ar
         break;
     }
 
+    for(i = 0; i < PARTY_SIZE; i++)
+    {
+        if(GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) == SPECIES_NONE)
+        {
+            if(i != 0)
+        dynamicLevel /= i;
+            break;
+        }
+        dynamicLevel += GetMonData(&gPlayerParty[i], MON_DATA_LEVEL);
+        if(i == 0)
+        {
+            LevelSpread[0], LevelSpread[1] = GetMonData(&gPlayerParty[i], MON_DATA_LEVEL);
+        }
+        else
+        {
+            u8 LevelCheck = GetMonData(&gPlayerParty[i], MON_DATA_LEVEL);
+            if(LevelCheck < LevelSpread[0])
+                LevelSpread[0] = LevelCheck;
+            else if(LevelCheck > LevelSpread[1])
+            LevelSpread[1] = LevelCheck;
+        }
+    }
+
+    if(i == PARTY_SIZE)
+		dynamicLevel /= i;
+            /* The following is used to account for a player having one or two very weak Pokemon
+	       along with some very strong Pokemon. It weights the averaged level more towards the
+	       player's strongest Pokemon
+	    */
+	
+    PartyLevelAdjust = LevelSpread[1] - LevelSpread[0];
+
+    if(LevelSpread[1] - dynamicLevel < 10)
+    {
+        PartyLevelAdjust = 0;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 20)
+    {
+        PartyLevelAdjust /= 10;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 30)
+    {
+        PartyLevelAdjust /= 5;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 40)
+    {
+        PartyLevelAdjust *= 3;
+        PartyLevelAdjust /= 10;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 50)
+    {
+        PartyLevelAdjust *= 2;
+        PartyLevelAdjust /= 5;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 60)
+    {
+        PartyLevelAdjust /= 2;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 70)
+    {
+        PartyLevelAdjust *= 3;
+        PartyLevelAdjust /= 5;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 80)
+    {
+        PartyLevelAdjust *= 7;
+        PartyLevelAdjust /= 10;
+    }
+    else if(LevelSpread[1] - dynamicLevel < 90)
+    {
+        PartyLevelAdjust *= 4;
+        PartyLevelAdjust /= 5;
+    }
+
     level = ChooseWildMonLevel(&wildMonInfo->wildPokemon[timeOfDay][wildMonIndex]);
     if (flags & WILD_CHECK_REPEL && !IsWildLevelAllowedByRepel(level))
         return FALSE;
