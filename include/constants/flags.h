8c6542db6c449b6d24d26e03d27c52f95d92c5ba
lemondrop81 <hacker814@hotmail.com>
2022-03-07 10:56:58 -0600

    Added axe to cut trees

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -513,7 +513,7 @@
 
 #define FLAG_DRILL                           0x1DE 
 #define FLAG_SPINACH                         0x1DF
-#define FLAG_UNUSED_0x1E0                    0x1E0 // Unused Flag
+#define FLAG_AXE                             0x1E0
 #define FLAG_UNUSED_0x1E1                    0x1E1 // Unused Flag
 #define FLAG_UNUSED_0x1E2                    0x1E2 // Unused Flag
 #define FLAG_UNUSED_0x1E3                    0x1E3 // Unused Flag
