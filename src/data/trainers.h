10e5260b2d294e52665618d878d9cb731a98cbc1
lemondrop81 <hacker814@hotmail.com>
2022-06-30 12:27:32 -0500

    Fix typo

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -5736,7 +5736,7 @@ const struct Trainer gTrainers[] = {
         .doubleBattle = FALSE,
         .aiFlags =  AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY | AI_FLAG_CHECK_FOE | AI_FLAG_HP_AWARE | AI_FLAG_SMART_SWITCHING,
         .partySize = ARRAY_COUNT(sParty_Robert2),
-        .party = {.NoItemDefaultMoves = sParty_Robert2},
+        .party = {.NoItemCustomMoves = sParty_Robert2},
     },
 
     [TRAINER_ROBERT_3] =
