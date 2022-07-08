0eb5cb2e259ec06b431e95f8afbd3745b900ca58
lemondrop81 <hacker814@hotmail.com>
2022-07-08 07:36:23 -0500

    Fix typo for lorenzo

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -7752,7 +7752,7 @@ const struct Trainer gTrainers[] = {
         .doubleBattle = FALSE,
         .aiFlags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY | AI_FLAG_CHECK_FOE | AI_FLAG_HP_AWARE | AI_FLAG_SMART_SWITCHING,
         .partySize = ARRAY_COUNT(sParty_Lorenzo),
-        .party = {.ItemCustomMoves = sParty_Lorenzo},
+        .party = {.NoItemCustomMoves = sParty_Lorenzo},
     },
 
     [TRAINER_SEBASTIAN] =
