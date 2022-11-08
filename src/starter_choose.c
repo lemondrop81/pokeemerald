e4c2ec6529ab54e8be825dfc5b539e837038ed4d
lemondrop81 <hacker814@hotmail.com>
2022-11-08 07:27:24 -0600

    Update starter choice, and tm case usage

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -166,9 +166,9 @@ static const u16 sStarterMonAlola[STARTER_MON_COUNT] =
 
 static const u16 sStarterMonMystery[STARTER_MON_COUNT] =
 {
-    SPECIES_NECROZMA_DUSK_MANE,
+    SPECIES_DELTA_SQUIRTLE,
     SPECIES_DELTA_BULBASAUR,
-    SPECIES_MARSHADOW,
+    SPECIES_DELTA_CHARMANDER,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
